2153b8a5b0955399cc633af4b1d59068b8a7bd6b
北邙 <communism_1921@163.com>
2024-04-08 17:55:37 +0800

    2024-04-08 17:55:36

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function filterItems() {
+  var filterValue = document.getElementById('filter').value;
+  var items = document.querySelectorAll('.item');
+  var filteredCount = 0;
+
+items.forEach(function(item) {
+if (filterValue === 'all') {
+  item.style.display = 'block';
+  filteredCount++;
+} else if (filterValue === 'cattox') {
+  if (item.classList.contains('cat') || item.classList.contains('both')) {
+    item.style.display = 'block';
+    filteredCount++;
+  } else {
+    item.style.display = 'none';
+  }
+} else if (filterValue === 'dogtox') {
+  if (item.classList.contains('dog') || item.classList.contains('both')) {
+    item.style.display = 'block';
+    filteredCount++;
+  } else {
+    item.style.display = 'none';
+  }
+} else if (filterValue === 'tox') {
+  if (item.classList.contains('dog') || item.classList.contains('cat') || item.classList.contains('both')) {
+    item.style.display = 'block';
+    filteredCount++;
+  } else {
+    item.style.display = 'none';
+  }
+}
+else {
+  if (item.classList.contains(filterValue)) {
+    item.style.display = 'block';
+    filteredCount++;
+  } else {
+    item.style.display = 'none';
+  }
+}
+});
+document.getElementById('filteredCount').textContent = filteredCount;
+}
+window.onload = filterItems;
+document.getElementById('filter').addEventListener('change', filterItems);
